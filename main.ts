6e5a8814acc9c9ed856d9a6ba5527d3437e9704f
Ariska Hidayat <ariska138@gmail.com>
2024-01-30 21:44:27 +0700

    fix name

--- main.ts
+++ main.ts
@@ -6,7 +6,8 @@ for await (const f of Deno.readDir('./public')) {
   const ext = f.name.split('.')[1];
   if (ext !== "mp3") continue;
   console.log(f.name, ext); //name of the file
-  data = { ...data, [idx]: "https://quran-api.deno.dev/" + f.name };
+  const index = f.name.split('QuranTerjemah-')[1].substring(0, 3);
+  data = { ...data, [index]: "https://quran-api.deno.dev/" + f.name };
   idx++;
 }
 
